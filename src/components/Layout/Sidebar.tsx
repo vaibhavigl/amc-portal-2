b713be315d762dfcdee11da16b0215437a43b9ae
Parikshit2304 <iamparikshit2304@gmail.com>
2025-07-27 12:01:24 +0530

    Fixed sidebar padding

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -56,7 +56,7 @@ const Sidebar: React.FC = () => {
         </ul>
       </nav>
 
-      <div className="absolute bottom-6 left-3 right-3 max-w-60">
+      <div className="absolute bottom-6 left-3 right-3 max-w-60 pr-2">
         <div className="bg-gradient-to-r from-blue-50 to-indigo-50 rounded-lg p-4 border border-blue-100">
           <div className="flex items-center space-x-3">
             <div className="w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center">
